0f288e84bd11bf57bad4e1844f4705126a8fbc1c
danialryan06-create <danialryan06@gmail.com>
2025-09-22 00:00:41 -0400

    Create AUList.h

---
+++ AUList.h
@@ -0,0 +1,38 @@
+#ifndef AULIST_H 
+#define AULIST_H 
+ 
+#define MAXSIZE 10 
+ 
+ 
+class AUList { 
+public: 
+  AUList(); 
+ 
+  void MakeEmpty(); 
+ 
+  bool IsFull() const; 
+ 
+  int GetLength() const; 
+  int GetItem(int); 
+ 
+  void PutItem(int); 
+ 
+  void DeleteItem(int); 
+ 
+  void ResetList(); 
+ 
+  void PrintList(); 
+ 
+  int GetNextItem(); 
+ 
+  int getMin(); 
+  int getRange(); 
+  AUList DuplicateSE(int first, int last); 
+ 
+private: 
+  int length; 
+  int ListItems[MAXSIZE]; 
+  int currentPos; 
+}; 
+ 
+#endif 
