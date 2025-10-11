d62e69c50ff4a71e22cda20e8f30255e3c521472
danialryan06-create <danialryan06@gmail.com>
2025-10-11 00:01:45 -0400

    Update LLUList.h

--- LLUList.h
+++ LLUList.h
@@ -1,37 +1,29 @@
-struct LNode; 
- 
-//Remember that header files should contain DECLARATIONS, but not DEFINITIONS for the 
-associated class functions. 
-class LLUList 
-{ 
-public: 
-   
- LLUList(); // Constructor 
-   
- ~LLUList(); // Destructor 
-   
- void MakeEmpty();  // Returns the list to the empty state. 
-     
- bool IsFull() const; //Determines whether list is full. (Precondition -- list has been 
-initialized).  Return value of "True" indicates class is full. 
-   
- int GetLength() const; //Determines the number of elements in list. 
- 
-  int GetItem(int); //Retrieves position of list element matching input item (if 
-present).  If not present, -1 is returned. 
- 
-  void PutItem(int); //Adds the item to the list. 
-   
- void DeleteItem(int); //Deletes the element whose key matches item's key. 
-   
- void ResetList();  //Initializes iterator's current position for an iteration through 
-the list. 
-   
- void PrintList();  //Print all elements of the list in a readable format. 
-int GetNextItem(); //Gets the next element in the list. 
-int getMin(); 
-int getRange(); 
-private: 
-LNode* ListStart;   
-LNode* curPos; 
+#ifndef LLULIST_H
+#define LLULIST_H
+
+struct LNode;
+
+class LLUList {
+public:
+  LLUList();
+  ~LLUList();
+
+  void MakeEmpty();
+  bool IsFull() const;
+  int GetLength() const;
+  int GetItem(int);
+  void PutItem(int);
+  void DeleteItem(int);
+  void ResetList();
+  void PrintList();
+  int GetNextItem();
+
+  int getMin();   // Added
+  int getRange(); // Added
+
+private:
+  LNode* ListStart;
+  LNode* curPos;
 };
+
+#endif
