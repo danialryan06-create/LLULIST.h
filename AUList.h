dfa8fb6874c9f59cf67b325a5bea981dcbec7621
danialryan06-create <danialryan06@gmail.com>
2025-10-11 00:03:37 -0400

    Update AUList.h

--- AUList.h
+++ AUList.h
@@ -1,38 +1,30 @@
-#ifndef AULIST_H 
-#define AULIST_H 
- 
-#define MAXSIZE 10 
- 
- 
-class AUList { 
-public: 
-  AUList(); 
- 
-  void MakeEmpty(); 
- 
-  bool IsFull() const; 
- 
-  int GetLength() const; 
-  int GetItem(int); 
- 
-  void PutItem(int); 
- 
-  void DeleteItem(int); 
- 
-  void ResetList(); 
- 
-  void PrintList(); 
- 
-  int GetNextItem(); 
- 
-  int getMin(); 
-  int getRange(); 
-  AUList DuplicateSE(int first, int last); 
- 
-private: 
-  int length; 
-  int ListItems[MAXSIZE]; 
-  int currentPos; 
-}; 
- 
-#endif 
+#ifndef AULIST_H
+#define AULIST_H
+
+#define MAXSIZE 10
+
+class AUList {
+public:
+  AUList();
+
+  void MakeEmpty();
+  bool IsFull() const;
+  int GetLength() const;
+  int GetItem(int);
+  void PutItem(int);
+  void DeleteItem(int);
+  void ResetList();
+  void PrintList();
+  int GetNextItem();
+
+  int getMin();              // Added
+  int getRange();            // Added
+  AUList DuplicateSE(int first, int last); // Added
+
+private:
+  int length;
+  int ListItems[MAXSIZE];
+  int currentPos;
+};
+
+#endif
