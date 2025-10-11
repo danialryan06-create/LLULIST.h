001111c5e13516542d2a292066a514b774f4be69
danialryan06-create <danialryan06@gmail.com>
2025-10-11 00:44:33 -0400

    Update AUList.cpp

--- AUList.cpp
+++ AUList.cpp
@@ -1,9 +1,13 @@
-#include "AUList.h"
 #include <iostream>
+#include <limits>
+#include "AUList.h"
+using namespace std;
 
-// Constructor with default capacity
-AUList::AUList(int cap) : capacity(cap), length(0), currentPos(-1) {
+AUList::AUList(int cap) {
+    capacity = cap;
+    length = 0;
     items = new int[capacity];
+    currentPos = 0;
 }
 
 void AUList::MakeEmpty() {
@@ -18,57 +22,85 @@ int AUList::GetLength() const {
     return length;
 }
 
-int AUList::GetItem(int gitem) const {
-    if (gitem >= 0 && gitem < length)
-        return items[gitem];
-    // Handle error if needed
-    return -1; // or throw exception
+int AUList::GetItem(int index) {
+    if (index >= 0 && index < length)
+        return items[index];
+    else
+        return -1; // or throw exception
 }
 
 void AUList::PutItem(int item) {
     if (!IsFull()) {
-        items[length] = item;
-        length++;
+        items[length++] = item;
     }
-    // else handle overflow
 }
 
 void AUList::DeleteItem(int item) {
+    int index = -1;
     for (int i = 0; i < length; i++) {
         if (items[i] == item) {
-            // Shift left remaining items
-            for (int j = i; j < length - 1; j++) {
-                items[j] = items[j + 1];
-            }
-            length--;
+            index = i;
             break;
         }
+    }
+
+    if (index != -1) {
+        for (int i = index; i < length - 1; i++) {
+            items[i] = items[i + 1];
+        }
+        length--;
     }
 }
 
 void AUList::ResetList() {
-    currentPos = -1;
+    currentPos = 0;
 }
 
 int AUList::GetNextItem() {
-    currentPos++;
     if (currentPos < length)
-        return items[currentPos];
+        return items[currentPos++];
     else
-        return -1; // or handle end of list
+        return -1; // or throw exception
 }
 
-void AUList::PrintList() const {
-    for (int i = 0; i < length; i++) {
-        std::cout << items[i] << " ";
+void AUList::PrintList() {
+    for (int i = 0; i < length; i++)
+        cout << items[i] << " ";
+    cout << endl;
+}
+
+
+int AUList::getMin() {
+    if (length == 0) return -1;
+
+    int min = items[0];
+    for (int i = 1; i < length; i++) {
+        if (items[i] < min) {
+            min = items[i];
+        }
     }
-    std::cout << std::endl;
+    return min;
 }
 
-AUList AUList::DuplicateSE(int first, int last) const {
-    AUList newList(last - first + 1);
-    for (int i = first; i <= last && i < length; i++) {
+
+int AUList::getRange() {
+    if (length == 0) return -1;
+
+    int min = items[0];
+    int max = items[0];
+    for (int i = 1; i < length; i++) {
+        if (items[i] < min) min = items[i];
+        if (items[i] > max) max = items[i];
+    }
+    return max - min;
+}
+
+
+AUList AUList::DuplicateSE(int start, int end) {
+    AUList newList(end - start + 1);
+    for (int i = start; i <= end && i < length; i++) {
         newList.PutItem(items[i]);
     }
     return newList;
 }
+
