a960539172fe64839bea47ee0e08c3923bce01d1
danialryan06-create <danialryan06@gmail.com>
2025-10-11 00:00:07 -0400

    Update LLUList.cpp

--- LLUList.cpp
+++ LLUList.cpp
@@ -7,163 +7,137 @@ struct LNode {
     LNode* next; 
 }; 
  
-LLUList::LLUList() { 
-  ListStart=NULL;   
-} 
- 
+LLUList::LLUList() {
+  ListStart = nullptr;
+}
+
 LLUList::~LLUList() {
+  MakeEmpty();
+}
+
+void LLUList::MakeEmpty() {
   LNode* tempPtr;
-  while (ListStart != NULL) {
+  while (ListStart != nullptr) {
     tempPtr = ListStart;
     ListStart = ListStart->next;
     delete tempPtr;
   }
 }
- 
-bool LLUList::IsFull() const { 
-  LNode* testNode; 
-  try { 
-    testNode = new LNode; 
-    delete testNode; 
-    return false; 
-  } 
-  catch(std::bad_alloc exception) { 
-    return true; 
-  } 
-} 
- 
-int LLUList::GetLength() const { 
-  LNode* LenPos = ListStart; 
-  int length=0; 
-  while (LenPos != NULL) { 
-    LenPos=LenPos->next; 
-    length++; 
-  } 
-  return length; 
-}; 
- 
-int LLUList::GetItem(int gitem) { //Procedure is similar to that of AUList, although the 
-relevant data is stored different. 
-                           //also, it's better for LLUList to proceed in order from the 
-beginning 
-   int position=0; 
-   LNode* nodeIter; 
-   nodeIter=ListStart; 
-   while ((nodeIter != NULL) && (nodeIter->item !=gitem)) { 
-      nodeIter=nodeIter->next; 
-      position++; } 
-   if (nodeIter==NULL) 
-      return -1; 
-   else 
-      return position; 
-} 
- 
-void LLUList::MakeEmpty() { //Unlike the AUList implementation, we need to step through 
-and delete EVERY node 
-  LNode* tempPtr; 
-  while (ListStart != NULL) { 
-    tempPtr = ListStart; 
-    ListStart = ListStart->next; 
-    delete tempPtr; 
-  }   
-} 
- 
- 
-void LLUList::PutItem(int newitem) { 
-  LNode* newNode; //Get a pointer          
- newNode = new LNode; //Create a new node.  
- newNode->item = newitem; // Store the item in the node 
-  newNode->next = ListStart; // Store address of next node  
- ListStart = newNode; // Redirect start of list 
-} 
- 
-void LLUList::DeleteItem(int ditem) {
-  LNode *tmpNode, *nodeIter;
 
+ 
+bool LLUList::IsFull() const {
+  try {
+    LNode* testNode = new LNode;
+    delete testNode;
+    return false;
+  } catch (std::bad_alloc&) {
+    return true;
+  }
+}
+ 
+int LLUList::GetLength() const {
+  int length = 0;
+  LNode* ptr = ListStart;
+  while (ptr != nullptr) {
+    ptr = ptr->next;
+    length++;
+  }
+  return length;
+}
+
+int LLUList::GetItem(int gitem) {
+  int position = 0;
+  LNode* nodeIter = ListStart;
+  while ((nodeIter != nullptr) && (nodeIter->item != gitem)) {
+    nodeIter = nodeIter->next;
+    position++;
+  }
+  return (nodeIter == nullptr) ? -1 : position;
+}
+
+void LLUList::PutItem(int newitem) {
+  LNode* newNode = new LNode;
+  newNode->item = newitem;
+  newNode->next = ListStart;
+  ListStart = newNode;
+}
+
+void LLUList::DeleteItem(int ditem) {
   if (ListStart == nullptr) return;
 
+  LNode *tmpNode;
   if (ListStart->item == ditem) {
     tmpNode = ListStart;
     ListStart = ListStart->next;
-    delete tmpNode;
-    return;
+  } else {
+    LNode* nodeIter = ListStart;
+    while (nodeIter->next != nullptr && nodeIter->next->item != ditem) {
+      nodeIter = nodeIter->next;
+    }
+    if (nodeIter->next == nullptr) return;
+    tmpNode = nodeIter->next;
+    nodeIter->next = nodeIter->next->next;
   }
-
-  nodeIter = ListStart;
-  while (nodeIter->next != nullptr && nodeIter->next->item != ditem)
-    nodeIter = nodeIter->next;
-
-  if (nodeIter->next == nullptr)
-    return; // Item not found
-
-  tmpNode = nodeIter->next;
-  nodeIter->next = nodeIter->next->next;
   delete tmpNode;
 }
- 
- 
-void LLUList::ResetList() { 
-  curPos=ListStart; 
-} 
- 
-int LLUList::GetNextItem() { 
-  int myitem; 
-  if (curPos == NULL) //The implemented option throw an errors if someone tries to access 
-an item past the last one.  
-   throw(38); //EOS Exception 
-  myitem = curPos->item; 
-  curPos=curPos->next; 
-  return myitem; 
-}; 
- 
-void LLUList::PrintList() { 
-  LNode* printPtr; 
-  printPtr=ListStart; 
-  std::cout<<"("; 
-  while (printPtr != NULL) { 
-    std::cout<<printPtr->item; 
-    if (printPtr->next!=NULL) 
-      std::cout<<", "; 
-    printPtr=printPtr->next; 
-  }     
- std::cout<<")"<<std::endl; 
-} 
- 
-int LLUList::getMin() { 
-  if (ListStart == nullptr) { 
-    std::cerr << "Error: List is empty." << std::endl; 
-    return -1; 
-  } 
- 
-  int minVal = ListStart->item; 
-  LNode* current = ListStart->next; 
- 
-  while (current != nullptr) { 
-    if (current->item < minVal) 
-      minVal = current->item; 
-    current = current->next; 
-  } 
- 
-  return minVal; 
-} 
- 
-int LLUList::getRange() { 
-  if (ListStart == nullptr) { 
-    std::cerr << "Error: List is empty." << std::endl; 
-    return -1; 
-  } 
- 
-  int minVal = ListStart->item; 
-  int maxVal = ListStart->item; 
-  LNode* current = ListStart->next; 
- 
-  while (current != nullptr) { 
-    if (current->item < minVal) 
-      minVal = current->item; 
-    if (current->item > maxVal) 
-      maxVal = current->item; 
-    current = current->next; 
-  } 
- 
-  return maxVal - minVal; 
-} 
+
+void LLUList::ResetList() {
+  curPos = ListStart;
+}
+
+int LLUList::GetNextItem() {
+  if (curPos == nullptr)
+    throw 38; // EOS Exception
+
+  int item = curPos->item;
+  curPos = curPos->next;
+  return item;
+}
+
+void LLUList::PrintList() {
+  LNode* printPtr = ListStart;
+  std::cout << "(";
+  while (printPtr != nullptr) {
+    std::cout << printPtr->item;
+    if (printPtr->next != nullptr)
+      std::cout << ", ";
+    printPtr = printPtr->next;
+  }
+  std::cout << ")" << std::endl;
+}
+
+int LLUList::getMin() {
+  if (ListStart == nullptr) {
+    std::cerr << "Error: List is empty." << std::endl;
+    return -1;
+  }
+
+  int minVal = ListStart->item;
+  LNode* current = ListStart->next;
+  while (current != nullptr) {
+    if (current->item < minVal)
+      minVal = current->item;
+    current = current->next;
+  }
+  return minVal;
+}
+
+int LLUList::getRange() {
+  if (ListStart == nullptr) {
+    std::cerr << "Error: List is empty." << std::endl;
+    return -1;
+  }
+
+  int minVal = ListStart->item;
+  int maxVal = ListStart->item;
+  LNode* current = ListStart->next;
+  while (current != nullptr) {
+    if (current->item < minVal)
+      minVal = current->item;
+    if (current->item > maxVal)
+      maxVal = current->item;
+    current = current->next;
+  }
+
+  return maxVal - minVal;
+}
