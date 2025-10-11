5f821840c58d8db3760bed581f2aa5c296b51d00
danialryan06-create <danialryan06@gmail.com>
2025-10-11 00:38:02 -0400

    Update AUList.h

--- AUList.h
+++ AUList.h
@@ -1,26 +1,26 @@
 #ifndef AULIST_H
 #define AULIST_H
 
-#include <stdexcept>  // for std::runtime_error
-
 class AUList {
-private:
-    int* ListItems;   // pointer to array holding elements
-    int length;       // current number of elements
-    int capacity;     // max capacity of the array
-
 public:
-    AUList(int cap = 100);   // constructor
-    ~AUList();               // destructor
+    AUList(int cap = 100);   // Constructor with default parameter
+    
+    void MakeEmpty();
+    bool IsFull() const;
+    int GetLength() const;
+    int GetItem(int gitem) const;
+    void PutItem(int item);
+    void DeleteItem(int item);
+    void ResetList();
+    int GetNextItem();
+    void PrintList() const;
+    AUList DuplicateSE(int first, int last) const;
 
-    int getLength() const { return length; }
-    int getMin();
-    int getRange();
-
-    // Other member functions you may have like add, remove, etc.
-
-    // For the DuplicateSE task (not included here)
-    // AUList DuplicateSE(int first, int last);
+private:
+    int* items;
+    int length;
+    int capacity;
+    int currentPos;
 };
 
 #endif
