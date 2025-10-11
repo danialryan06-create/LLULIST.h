c92282d09ea95c6118f1a15b5795041f6aa94e1e
danialryan06-create <danialryan06@gmail.com>
2025-10-11 00:15:39 -0400

    Update AUList.h

--- AUList.h
+++ AUList.h
@@ -10,16 +10,16 @@ public:
     void MakeEmpty();
     bool IsFull() const;
     int GetLength() const;
-    int GetItem(int); // returns index of item or -1
+    int GetItem(int); // Returns index of item or -1
     void PutItem(int);
     void DeleteItem(int);
     void ResetList();
     void PrintList();
     int GetNextItem();
 
-    int getMin();
-    int getRange();
-    AUList DuplicateSE(int first, int last);
+    int getMin();         
+    int getRange();        
+    AUList DuplicateSE(int first, int last);  
 
 private:
     int length;
